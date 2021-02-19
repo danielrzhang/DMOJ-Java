e99e0ea1336c3681cbf5ba668fb7aa1918f506f9
Daniel Zhang <45671923+danielrzhang@users.noreply.github.com>
2021-02-19 18:31:41 -0500

    Add files via upload

---
+++ Mock CCC/mockccc21s1.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MockCCCS1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		String alphabet = in.readLine();
+		String str = in.readLine();
+		ArrayList<String> wordList = new ArrayList<String>();
+
+		String addString, changeString, removeString;
+		for (int i = 0; i < alphabet.length(); i++) {
+			addString = "";
+			changeString = "";
+			removeString = "";
+
+			for (int j = 0; j <= str.length(); j++) {
+				addString = str.substring(0, j) + alphabet.charAt(i) + str.substring(j);
+				if (!wordList.contains(addString)) {
+					wordList.add(addString);
+				}
+			}
+
+			for (int j = 0; j < str.length(); j++) {
+				changeString = str.substring(0, j) + alphabet.charAt(i) + str.substring(j + 1);
+				removeString = str.substring(0, j) + str.substring(j + 1);
+
+				if (!wordList.contains(changeString) && !changeString.equals(str)) {
+					wordList.add(changeString);
+				}
+
+				if (!wordList.contains(removeString)) {
+					wordList.add(removeString);
+				}
+			}
+		}
+		Collections.sort(wordList);
+		for (int i = 0; i < wordList.size(); i++) {
+			System.out.println(wordList.get(i));
+		}
+	}
+}
---
+++ Mock CCC/mockccc21s2.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class MockCCCS2 {
+
+	private static ArrayList<String> list = new ArrayList<String>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = in.readLine();
+		findValidSubsequences(str, "");
+
+		String element;
+
+		for (int i = 0; i < list.size(); i++) {
+			element = list.get(i);
+			if (checkHasDuplicate(element)) {
+				list.remove(element);
+				i--;
+			}
+		}
+		System.out.println(list.size() % (int) (Math.pow(10, 9) + 7));
+	}
+
+	public static void findValidSubsequences(String str, String subSequence) {
+		if (str.length() == 0) {
+			if (!list.contains(subSequence)) {
+				list.add(subSequence);
+			}
+			return;
+		} else {
+			findValidSubsequences(str.substring(1), subSequence + str.charAt(0));
+			findValidSubsequences(str.substring(1), subSequence);
+		}
+	}
+
+	public static boolean checkHasDuplicate(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = i + 1; j < str.length(); j++) {
+				if (str.charAt(i) == str.charAt(j)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
