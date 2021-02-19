6bfa32a53b50beef3002226fe9582108a700cd97
Daniel Zhang <45671923+danielrzhang@users.noreply.github.com>
2021-02-19 18:32:50 -0500

    Add files via upload

---
+++ Other/KeenKeenerSequence.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+
+public class KeenKeenerSequence {
+
+	public static void main(String[] args) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+
+	}
+
+}
