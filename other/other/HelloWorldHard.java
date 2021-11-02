fa10075bf345ee081f2006d7cb5c2cea48f0eb41
Daniel Zhang <45671923+danielrzhang@users.noreply.github.com>
2021-11-02 11:01:27 -0400

    Delete other directory

--- other/other/HelloWorldHard.java
+++
@@ -1,8 +0,0 @@
-package other;
-public class HelloWorldHard {
-	public static void main(String[] args) {
-		System.out.println("Hello, World!"); System.out.println("Hello, World!"); System.out.println("Hello, World!");
-		System.out.println("Hello, World!"); System.out.println("Hello, World!"); System.out.println("Hello, World!");
-		System.out.println("Hello, World!"); System.out.println("Hello, World!"); System.out.println("Hello, World!");
-	}
-}
